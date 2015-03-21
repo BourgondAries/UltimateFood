0278e197afe36a889cfeae68e9ff8edf2c7475c5
Bourgond Aries <macocio@gmail.com>
2015-03-21 05:06:48 +0100

    Add loadDatabase

---
+++ loadDatabase.m
@@ -0,0 +1,11 @@
+%{
+    Loads the database from foods.txt.
+    foods.txt will contain the structure
+    for the parsing algorithm.
+%}
+function [nutrients, food_names, database] = loadDatabase()
+    nutrients = 0;
+    food_names = 0;
+    database = 0;
+end
+
