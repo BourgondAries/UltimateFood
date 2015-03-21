af00779898e909d8574a2b58bc8c8efa44b731ec
oaramsda <olea.ramsdal@gmail.com>
2015-03-21 21:49:21 +0100

    improve indentation

--- loadProfiles.m
+++ loadProfiles.m
@@ -1,34 +1,34 @@
 function [profile_names, values] = loadProfiles( )
 
-profile_names = {};
-profiles = [];
-source = 'profiles.txt';
-values = load(source);
-fileID = fopen(source);
+    profile_names = {};
+    profiles = [];
+    source = 'profiles.txt';
+    values = load(source);
+    fileID = fopen(source);
 
-line = '%';
-while strcmp(line(1), '%')
-   line = fgets(fileID);
-end
-
-while ~feof(fileID)
-
-    line = fgets(fileID);
-    line = line(2:end);
-    if length(profiles) == 0
-        profiles = [profiles line];
-    else
-        profiles = [profiles ' ' line];
+    line = '%';
+    while strcmp(line(1), '%')
+       line = fgets(fileID);
     end
-    fgets(fileID);
-    fgets(fileID);
-end
 
-profile_names = strsplit(profiles, '\n');
-profile_names = strtrim(profile_names);
+    while ~feof(fileID)
 
-profile_names(length(profile_names)) = [];
-fclose(fileID);
+        line = fgets(fileID);
+        line = line(2:end);
+        if length(profiles) == 0
+            profiles = [profiles line];
+        else
+            profiles = [profiles ' ' line];
+        end
+        fgets(fileID);
+        fgets(fileID);
+    end
+
+    profile_names = strsplit(profiles, '\n');
+    profile_names = strtrim(profile_names);
+
+    profile_names(length(profile_names)) = [];
+    fclose(fileID);
 
 end
 
--- saveDatabase.m
+++ saveDatabase.m
@@ -1,39 +1,38 @@
 function saveDatabase(cell_array, nutrients )
 
-fileID = fopen('test.txt', 'w'); %Change 'test.txt' to desired file
+    fileID = fopen('test.txt', 'w'); %Change 'test.txt' to desired file
 
-fprintf(fileID,'%s This is the info text\n', '%');
-fprintf(fileID, '\n');
-nuts_str = '';
-for i=1:length(nutrients)
-    if i==1
-        nuts_str = [nuts_str, sprintf('%s%s\t', '%', nutrients{i})];
-    else
-        nuts_str = [nuts_str, sprintf('%s\t', nutrients{i})];
-    end
-end
-fprintf(fileID,'%s',nuts_str);
-
-fprintf(fileID,'\n');
-sizes = size(cell_array);
-rows = sizes(2);
-cols = sizes(1);
-for i=1:rows
-    fprintf(fileID,'%s%s\n','%', cell_array{i,1});
-    line = '';
-    for j=3:cols
-        if j==3
-            line = strcat(line, sprintf(' %.0f\t', cell_array{i,j}));
+    fprintf(fileID,'%s This is the info text\n', '%');
+    fprintf(fileID, '\n');
+    nuts_str = '';
+    for i=1:length(nutrients)
+        if i==1
+            nuts_str = [nuts_str, sprintf('%s%s\t', '%', nutrients{i})];
         else
-             line = strcat(line, sprintf(' %.4f\t', cell_array{i,j}));
+            nuts_str = [nuts_str, sprintf('%s\t', nutrients{i})];
         end
-    
     end
-    fprintf(fileID,'%s\n',nuts_str);
-    fprintf(fileID,'%s\n',line);
+    fprintf(fileID,'%s',nuts_str);
+
+    fprintf(fileID,'\n');
+    sizes = size(cell_array);
+    rows = sizes(2);
+    cols = sizes(1);
+    for i=1:rows
+        fprintf(fileID,'%s%s\n','%', cell_array{i,1});
+        line = '';
+        for j=3:cols
+            if j==3
+                line = strcat(line, sprintf('%.0f\t', cell_array{i,j}));
+            else
+                 line = strcat(line, sprintf(' %.4f\t', cell_array{i,j}));
+            end
+
+        end
+        fprintf(fileID,'%s\n',nuts_str);
+        fprintf(fileID,'%s\n',line);
+    end
+
+    fclose(fileID);
+
 end
-
-fclose(fileID);
-
-end
-
